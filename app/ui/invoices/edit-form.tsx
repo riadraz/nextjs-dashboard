0eebb0c18f241d3bdffd70edaa93f5ed16529408
riadraz <riadraz@gmail.com>
2024-01-13 12:07:03 +0900

    20240116 adding some crud action

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice } from '@/app/lib/actions';
+
 import {
   CheckIcon,
   ClockIcon,
@@ -17,9 +19,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        <form action={updateInvoiceWithId}>
+        
+    <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
