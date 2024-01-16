e7382516e9f8cad18999081bd6ea8cb013d1d6a7
riadraz <riadraz@gmail.com>
2024-01-16 21:20:26 +0900

    from terminal 202401166

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -72,7 +72,7 @@ export async function updateInvoice(
   prevState: State,
   formData: FormData,
 ) {
-  const validatedFields = UpdateInvoice.safeParse({
+  const validatedFields = updateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
