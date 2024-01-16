fe717e9f2e373b960a74347f7a59fd8b699786ea
riadraz <riadraz@gmail.com>
2024-01-16 21:06:46 +0900

    from terminal 20240116

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -56,7 +56,7 @@ export default async function LatestInvoices() { // Remove props
         </div> }
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+          <h3 className="ml-2 text-sm text-gray-500 ">最新</h3>
         </div>
       </div>
     </div>
