a5d8d8e4d36c945d52623f8a23a447eacc77b46f
riadraz <riadraz@gmail.com>
2024-01-18 11:31:14 +0900

    ２０２４０１１８ jpnese text setup

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -29,7 +29,7 @@ export default function EditInvoiceForm({
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+          顧客選択
           </label>
           <div className="relative">
             <select
@@ -39,7 +39,7 @@ export default function EditInvoiceForm({
               defaultValue={invoice.customer_id}
             >
               <option value="" disabled>
-                Select a customer
+              選択してください
               </option>
               {customers.map((customer) => (
                 <option key={customer.id} value={customer.id}>
@@ -54,7 +54,7 @@ export default function EditInvoiceForm({
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+          金額を選択
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -75,7 +75,7 @@ export default function EditInvoiceForm({
         {/* Invoice Status */}
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
-            Set the invoice status
+          請求書のステータス
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
@@ -92,7 +92,7 @@ export default function EditInvoiceForm({
                   htmlFor="pending"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
                 >
-                  Pending <ClockIcon className="h-4 w-4" />
+                  保留中 <ClockIcon className="h-4 w-4" />
                 </label>
               </div>
               <div className="flex items-center">
@@ -108,7 +108,7 @@ export default function EditInvoiceForm({
                   htmlFor="paid"
                   className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
                 >
-                  Paid <CheckIcon className="h-4 w-4" />
+                  支払済み <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
             </div>
@@ -120,9 +120,9 @@ export default function EditInvoiceForm({
           href="/dashboard/invoices"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
-          Cancel
+          キヤンセル
         </Link>
-        <Button type="submit">Edit Invoice</Button>
+        <Button type="submit">請求編集</Button>
       </div>
     </form>
   );
